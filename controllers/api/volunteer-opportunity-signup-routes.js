dd546339aabc10cc701b2afd8b9bce2108750dc2
Shane Wade <shanebramble96@hotmail.com>
2025-06-27 18:50:42 -0500

    init () is able to return all volunteer profile ids who signup for a specific opportunity

--- controllers/api/volunteer-opportunity-signup-routes.js
+++ controllers/api/volunteer-opportunity-signup-routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { Signup } from "../../models/index.js";
+import { where } from "sequelize";
 
 const router = Router();
 // Get all volunteer opportunity signups
@@ -11,6 +12,27 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
+// Get all volunteers who signed up for a specific opportunity.
+router.get("/opportunity/:id", async (req, res) => {
+  try {
+    const signupResponse = await Signup.findAll({
+      where: {
+        opportunity_id: req.params.id,
+      },
+    });
+
+    if (signupResponse.length === 0) {
+      res.status(404).json({ message: "Cannot locate signups with this id!" });
+      return;
+    }
+
+    res.status(200).json(signupResponse);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 // Get a single volunteer opportunity signup by id
 router.get("/:id", async (req, res) => {
   try {
--- public/js/volunteer-signup.js
+++ public/js/volunteer-signup.js
@@ -1,11 +1,12 @@
+const opportunity_id = parseInt(window.location.pathname.split("/").pop());
+
 var volunteerSignupHandler = async function (event) {
   event.preventDefault();
-  const url = "http://localhost:3001/api/users/profile";
+  const profileEndpoint = "http://localhost:3001/api/users/profile";
   const signupEndPoint = "http://localhost:3001/api/signups";
-  const opportunity_id = window.location.pathname.split("/").pop();
 
   try {
-    const response = await fetch(url);
+    const response = await fetch(profileEndpoint);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
@@ -21,13 +22,26 @@ var volunteerSignupHandler = async function (event) {
     });
     const signupResult = await signupResponse.json();
     console.log(signupResult);
-    
-    
+  } catch (error) {
+    console.error(error.message);
+  }
+  // Reload after sucessfull post to signup model.
+  location.reload();
+};
+
+var init = async () => {
+  const url = `http://localhost:3001/api/signups/opportunity/${opportunity_id}`;
+  try {
+    const signupsResponse = await fetch(url);
+    const signups = await signupsResponse.json();
+
+    console.log(signups);
   } catch (error) {
     console.error(error.message);
   }
 };
 
+init();
 document
   .querySelector("form")
   .addEventListener("submit", volunteerSignupHandler);
