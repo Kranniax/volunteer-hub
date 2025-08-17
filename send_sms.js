e940647d2814eef85fe7453c7a7df5550093a057
Shane Wade <shanebramble96@hotmail.com>
2025-08-17 18:03:43 -0500

    Create SMS notification reminder

---
+++ send_sms.js
@@ -0,0 +1,20 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+import twilio from "twilio"; // Or, for ESM: import twilio from "twilio";
+import "dotenv/config";
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
+
+async function createMessage() {
+  const message = await client.messages.create({
+    body: "This is a test of the volunteer hub sms reminder",
+    from: "+18556389629",
+    to: "+18777804236",
+  });
+
+  console.log(message.body);
+}
+
+createMessage();
